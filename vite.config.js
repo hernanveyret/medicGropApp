b9b289c043ecd7edcad1640e8e86cca8541a4348
hernanveyret <hernanveyret@hotmail.com>
2025-05-18 02:37:57 -0300

    Correccion de errores

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     VitePWA({
-      registerType:'autoUpdate',
-      manifest:{
-        name:'MedicGroup',
-        short_name:'MedicGroup',
-        description:'App de Obra social',
-        theme_color:'#fffff',
-        background_color: "#ffffff",
-        display:'standalone',
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'MedicGroup',
+        short_name: 'MedicGroup',
+        description: 'App de Obra social',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
         icons: [
           {
-            src:'./icons/icons-192x192.png',
-            sizes:'192x192',
-            type:'image/png'
+            src: 'icons/icons-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            src:'./icons/icons-512x512.png',
-            sizes:'512x512',
-            type:'image/png'
+            src: 'icons/icons-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           }
         ]
       }
     })
-  ],
+  ]
 })
